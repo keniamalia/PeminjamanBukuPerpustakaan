85f8f0bcc50edaf6c2d8b63087c8fd67da6422d5
keniamalia <keniamalia1@gmail.com>
2016-09-29 14:32:45 +0700

    submit edit text

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4018/pemijamanbukuperpustakaan/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4018/pemijamanbukuperpustakaan/MainActivity.java
@@ -2,31 +2,57 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl4018.pemijamanbukuperpustakaan;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    /*    EditText etNama;
-        EditText etAlamat;
+    EditText etNama;
+    EditText etTelepon;
+    Button Btnsubmit;
         TextView tvHasil;
-        Button submit; */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-      /* etNama = (EditText) findViewById(R.id.editTextNama);
-        etAlamat = (EditText) findViewById(R.id.editTextAlamat);
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
-        submit = (Button) findViewById(R.id.btnsubmit);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        submit.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view)
-            {
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etTelepon = (EditText) findViewById(R.id.editTextTelepon);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        Btnsubmit = (Button) findViewById(R.id.btnsubmit);
+
+        Btnsubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 doProses();
 
             }
-        }); */
+        });
+    }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private void doProses() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            int tlp = Integer.parseInt(etTelepon.getText().toString());
+            tvHasil.setText("Nama: " + nama + "\n" + "No Telepon: " + tlp);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        //String tlp = etTelepon.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama harus diisi!");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+        return valid;
     }
 
 }
