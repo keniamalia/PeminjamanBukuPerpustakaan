db48bc351f8299c228d5856febe67f44336ca4ac
keniamalia <keniamalia1@gmail.com>
2016-09-29 10:37:20 +0700

    first commit : tampilan awal / interface

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4018/pemijamanbukuperpustakaan/MainActivity.java
@@ -0,0 +1,32 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl4018.pemijamanbukuperpustakaan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    /*    EditText etNama;
+        EditText etAlamat;
+        TextView tvHasil;
+        Button submit; */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+      /* etNama = (EditText) findViewById(R.id.editTextNama);
+        etAlamat = (EditText) findViewById(R.id.editTextAlamat);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        submit = (Button) findViewById(R.id.btnsubmit);
+
+        submit.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view)
+            {
+                doProses();
+
+            }
+        }); */
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
